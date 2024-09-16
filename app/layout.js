c08edd07763713dccb49aefe4f46675bf9fe71ba
Jyoti <jyoti.ogennavar@xecurify.com>
2024-09-16 14:59:45 +0530

    Refactor CSS styles and file structure

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import { ThemeProvider } from "@/components/ThemeProvider";
